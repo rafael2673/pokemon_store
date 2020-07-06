b396306b1569eb6d4564ca3b39f3705c9afe8db1
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-06 19:03:54 -0300

    Adicionando estilização e passando a propriedade de busca para o componente Pokemon

--- src/pages/components/Menu/index.tsx
+++ src/pages/components/Menu/index.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { FormEvent, useState, ChangeEvent } from 'react';
 import lupa from '../../../assets/search.svg';
 import './style.css';
+import Pokemons from '../Pokemons';
 
 export default function App() {
-  return (
-    <div className="nav">
-			<form className = "form">
-				<label><img src={lupa} alt = "lupa" className = "icon" /></label>
-				<input type="text" className = "pesquisar" name="pesquisar" placeholder="pesquisar" />
-			</form>
+	const [pesquisa, setPesquisa] = useState({ busca: '' });
+	const [busca, setBusca] = useState('');
+	
+	function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+		const valor = e.target.value;
+		setPesquisa({ ...pesquisa, busca: valor });
+	}
+	
+	function getBusca() {
+		return busca;
+	}
+	function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+		const { busca } = pesquisa;
+
+		setBusca(busca);
+
+	}
+	return (
+		<div className='container-fluid'>
+			<div className="nav">
+				<form className="form" onSubmit={handleSubmit}>
+					<label><img src={lupa} alt="lupa" className="icon" /></label>
+					<input type="text" className="pesquisar"
+						name="pesquisar" placeholder="pesquisar"
+						onChange={handleInputChange} />
+				</form>
+			</div>
+
+			<Pokemons busca = {getBusca()} />
 		</div>
-  );
+	);
 }
