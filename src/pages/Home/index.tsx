dbe1dbed0e6da57c4dea355f5891ae5c71059867
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-06 18:55:07 -0300

    Home atualizado para cumprir sua função como armazenador de componentes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Menu from '../components/Menu';
-import Pokemons from '../components/Pokemons';
-import Carrinho from '../components/Carrinho';
 
 import './style.css'
 
 export default function App() {
   return (
-    <div className = 'container-fluid'>
       <Menu />
-      <div className="row">
-        <Pokemons />
-        <Carrinho />
-      </div>
-    </div>
   );
 }
