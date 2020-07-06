9f35d98d2642226370124ef9330d18b5b70c6bdd
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-06 19:01:16 -0300

    Adicionando outras funcionalidades como paginação, correção de erros e busca por nome do pokemon

--- src/pages/components/Pokemons/index.tsx
+++ src/pages/components/Pokemons/index.tsx
@@ -1,40 +1,155 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../../service/api';
+import Sprites from './Sprites';
+import Carrinho from '../Carrinho';
+import Pagination from '../Pagination';
 
-export default function App() {
+import './style.css';
+
+interface Pokemons {
+  name: string,
+  url: string
+}
+interface Pokemon {
+  id: number,
+  name: string,
+  url: string
+}
+
+type Props = {
+  busca: string
+}
+
+export default function App({ busca }: Props) {
+  const [pokemons, setPokemons] = useState<Pokemons[]>([]);
+  const [pokemon, setPokemon] = useState<Pokemon>({ id: 0, name: '', url: '' });
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [ids, setIds] = useState(0);
+  const [url, setURL] = useState('');
+  const [urlPrev, setUrlPrev] = useState('');
+  useEffect(() => {
+
+    if (busca === null || busca === '') {
+      const pokemon: Pokemon = {id: 0, name: '', url: ''} 
+      setPokemon(pokemon);
+      api.get('pokemon/?limit=21').then(resp => {
+        setPokemons(resp.data.results);
+        setURL(resp.data.next);
+        setUrlPrev(resp.data.previous);
+      }
+      );
+    } else {
+
+      api.get(`pokemon/${busca}`).then(resp => {
+        console.log(resp.data);
+        const { id, name } = resp.data;
+        const sprite = resp.data.sprites.front_default;
+        const pokemon: Pokemon = { id: id, name: name, url: sprite }
+        setPokemon(pokemon);
+      }
+      );
+    }
+  }, [busca]);
+
+
+  function setId(id: number) {
+    setIds(id);
+  }
+  function getId() {
+    return ids;
+  }
+
+  function getUrl() {
+    return url.slice(26, 60);
+  }
+  function getUrlPrev() {
+    if (urlPrev !== null) {
+      return urlPrev.slice(26, 60);
+    } else {
+      return null;
+    }
+  }
+  function handleCarrinho(id: number) {
+    const alreadSelected = selectedItems.findIndex(item => item === id);
+    if (alreadSelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+  function handleOver() {
+    console.log("passou por cima")
+  }
+  function handleNext() {
+    api.get(getUrl()).then(resp => {
+      setPokemons(resp.data.results);
+      setURL(resp.data.next);
+      setUrlPrev(resp.data.previous);
+    }
+    );
+  }
+  function handlePrev() {
+    const url: string = getUrlPrev() as string;
+    if (url !== null) {
+      api.get(url).then(resp => {
+        setPokemons(resp.data.results);
+        setURL(resp.data.next);
+        setUrlPrev(resp.data.previous);
+      }
+      );
+    }
+  }
+
   return (
     <>
-      <div className="left col-lg-6 col-sm-6">
-        <div className="row">
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg" />
-          </div>
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://iloveflores.com/wp-content/uploads/2015/10/flores-lindas.jpg" />
-          </div>
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://imagens-revista.vivadecora.com.br/uploads/2018/05/tipos-de-flores-girassol.jpg" />
-          </div>
-        </div>
-        <div className="row">
 
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://www.mundodeflores.com/images/fotos-flores-3.jpg?phpMyAdmin=9ea091c51a5aa3cf876fb3cf0a5f7f3d" />
-          </div>
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://mobimg.b-cdn.net/pic/v2/gallery/preview/cvety-rasteniya-risunki-39817.jpg" />
-          </div>
-          <div className="col-sm">
-            <img className="img-fluid img img-thumbnail" alt = 'pokemon'
-              src="https://noticias.buscavoluntaria.com.br/wp-content/uploads/2019/07/irina-iriser-mNz9Pa3tz34-unsplash.jpg" />
+      <div className="row">
+        <div className="left col-lg-6 col-sm-6" onMouseOver={handleOver}>
+          <div className="row row-cols-1 row-cols-md-3">
+            {pokemons !== undefined && pokemon.id === 0 ? pokemons.map(result => (
+              <div key={result.url.split('/')[6]}
+                className="col mb-4">
+                <div onClick={() => setId(Number(result.url.split('/')[6]))}
+                  className={`card text-center 
+                  ${selectedItems.includes(Number(result.url.split('/')[6])) ? 'selected' : ''}`}>
+                  <div className="card-header">
+                    {result.url.split('/')[6]}
+                  </div>
+                  <div className="card-body">
+                    <Sprites url={result.url as string} />
+                    <p className="card-text">{result.name}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+              :
+              <div className="col mb-4">
+                <div onClick={() => setId(pokemon.id)}
+                  className={`card text-center 
+                  ${selectedItems.includes(pokemon.id) ? 'selected' : ''}`}>
+                  <div className="card-header">
+                    {pokemon.id}
+                  </div>
+                  <div className="card-body">
+                    <img src={pokemon.url} alt="pokemon" className="card-img-top" />
+                    <p className="card-text">{pokemon.name}</p>
+                  </div>
+                </div>
+              </div>
+
+
+            }
           </div>
         </div>
+        <div className="center col-lg-1 col-sm-1"></div>
+
+        <Carrinho handleCarrinho={handleCarrinho} getId={getId()} setId={setId} />
       </div>
-      <div className="center col-lg-1 col-sm-1"></div>
+      <Pagination handleNext={handleNext}
+        getUrlPrev={getUrlPrev} handlePrev={handlePrev}
+      />
     </>
   );
 }
