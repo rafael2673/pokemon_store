f27a74cedf1046e5e59b2855bf707ebf3e463b64
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-06 19:16:48 -0300

    Adição do preço no Catálogo

--- src/pages/components/Pokemons/Sprites.tsx
+++ src/pages/components/Pokemons/Sprites.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../../service/api';
 
-export default function App(props: any){
+export default function App(props: any) {
   const [sprite, setSprite] = useState<string>('');
+  const [price, setPrice] = useState<number>(0);
+
   useEffect(() => {
     api.get(props.url as string).then(response => {
       const imageURL = response.data.sprites.front_default as string;
+      const { base_experience } = response.data;
       setSprite(imageURL)
+      setPrice(base_experience);
     })
   }, [props])
 
   return (
+    <>
       <img src={sprite} alt="pokemon" className="card-img-top" />
+      <i>{`R$ ${price}`}</i>
+    </>
   );
 }
