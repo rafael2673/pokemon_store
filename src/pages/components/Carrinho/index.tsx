3c101081ca1be53a80cda15ba3c8dbb412c3a9a5
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-07 22:27:30 -0300

    Correção do total, implementando a soma do preço

--- src/pages/components/Carrinho/index.tsx
+++ src/pages/components/Carrinho/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../../service/api';
+import Modal from '../Modal';
 
 type CardProps = {
   getId: number,
-  handleCarrinho(id: number): void
-  setId(id: number): void
+  handleCarrinho(id: number): void,
+  setId(id: number): void,
+
 }
 interface Pokemons {
   id: number;
@@ -14,33 +16,46 @@ interface Pokemons {
 }
 export default function App({ getId, handleCarrinho, setId }: CardProps) {
   const [selectedPokemons, setSelectedPokemons] = useState<Pokemons[]>([]);
-
+  const [price, setPrice] = useState(0);
+  const [contador, setContador] = useState(0);
+  
+  
   useEffect(() => {
 
+    if ((selectedPokemons.length) - 1 === contador) {
+      setPrice(price + selectedPokemons[selectedPokemons.length - 1].price)
+      setContador(contador + 1);
+    }
     if (getId > 0) {
+      
       api.get(`pokemon/${getId}`).then(resp => {
         setId(0);
         const id: number = resp.data.id;
         const teste = selectedPokemons.findIndex(item => item.id === id);
         handleCarrinho(id);
 
+
         if (teste >= 0) {
           const filteredItems = selectedPokemons.filter(item => item.id !== id);
+          setPrice(price - selectedPokemons[teste].price);
+          setContador(contador - 1);
           setSelectedPokemons(filteredItems);
+
         } else {
-          const price: number = resp.data.base_experience;
+          const preco: number = resp.data.base_experience;
           setSelectedPokemons([...selectedPokemons, {
             id: resp.data.id,
             name: resp.data.name,
             sprites: resp.data.sprites.front_default,
-            price: price,
+            price: preco,
           }])
-
         }
       })
+      
     }
-    // console.log(selectedPokemons)
-  }, [getId, handleCarrinho, selectedPokemons, setId]);
+  }, [getId, handleCarrinho, selectedPokemons, setId, price, contador]);
+
+
   return (
     <div className="right col-lg-4 col-sm-4">
       <table className="table table-hover">
@@ -61,11 +76,11 @@ export default function App({ getId, handleCarrinho, setId }: CardProps) {
           )) : ''}
           <tr>
             <td colSpan={2}>Total</td>
-            <td>R$000</td>
+            <td>{`R$${price}`}</td>
           </tr>
           <tr>
             <td colSpan={3}>
-              <button type="button" className="btn btn-secondary w-100">Secondary</button>
+              <Modal selectedPokemons = {selectedPokemons}/>
             </td>
           </tr>
         </tbody>
